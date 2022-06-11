7f77b0a30ad60036b8670c9dc8cf08e8230a1b42
Ben Jesaulenko <design@upromotions.com.au>
2022-06-11 12:22:45 +1000

    Update posts.js

--- src/components/posts.js
+++ src/components/posts.js
@@ -2,16 +2,18 @@ import * as React from "react";
 import { List, Datagrid, TextField, ReferenceField, EditButton, Edit, Create, SimpleForm, ReferenceInput, SelectInput, TextInput, useRecordContext } from "react-admin";
 
 const PostTitle = () => {
-    const record = useRecordContext();
-    return <span>Post {record ? `"${record.title}"` : ''}</span>;
+  const record = useRecordContext();
+  return <span>Post Name: {record ? `"${record.title}"` : ''}</span>;
 }
-const EditPostTitle = () => {
-    const record = useRecordContext();
-    return <span>Edit Post {record ? `"${record.title}"` : ''}</span>;
-}
+const postFilters = [
+  <TextInput label="Search" source="q" alwaysOn />,
+  <ReferenceInput label="User" source="user_id" reference="users" allowEmpty>
+    <SelectInput optionText="name" />
+  </ReferenceInput>
+];
 
-export const PostList = (props) => (
-  <List>
+export const PostList = () => (
+  <List filters={postFilters}>
     <Datagrid>
       <TextField source="id" />
       <ReferenceField source="userId" reference="users">
@@ -24,26 +26,26 @@ export const PostList = (props) => (
 );
 
 export const PostEdit = (props) => (
-    <Edit title={<EditPostTitle />}>
-        <SimpleForm>
-           <TextInput disabled source="id" />
-            <ReferenceInput source="userId" reference="users">
-               <SelectInput optionText="name" sx={{ width: '100%',}} />
-            </ReferenceInput>
-            <TextInput source="title" sx={{ width: '100%',}} />
-           <TextInput multiline source="body" sx={{ width: '100%',}} />
-        </SimpleForm>
-    </Edit>
+  <Edit title={<PostTitle />}>
+    <SimpleForm>
+      <TextInput disabled source="id" />
+      <ReferenceInput source="userId" reference="users">
+        <SelectInput optionText="name" sx={{ width: '100%', }} />
+      </ReferenceInput>
+      <TextInput source="title" sx={{ width: '100%', }} />
+      <TextInput multiline source="body" sx={{ width: '100%', }} />
+    </SimpleForm>
+  </Edit>
 );
 
 export const PostCreate = (props) => (
   <Create {...props}>
     <SimpleForm>
       <ReferenceInput source="userId" reference="users">
-        <SelectInput optionText="name" sx={{ width: '100%',}} />
+        <SelectInput optionText="name" sx={{ width: '100%', }} />
       </ReferenceInput>
-      <TextInput source="title" sx={{ width: '100%',}} />
-      <TextInput multiline source="body" sx={{ width: '100%',}} />
+      <TextInput source="title" sx={{ width: '100%', }} />
+      <TextInput multiline source="body" sx={{ width: '100%', }} />
     </SimpleForm>
   </Create>
 );
